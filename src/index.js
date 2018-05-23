3d50d549b93a41c9b921aed652beecae938625fe
sh4hids <shahid.bracu@gmail.com>
2018-05-23 14:40:49 +0600

    Added first styled component and global styles

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { injectGlobal } from 'styled-components';
+
+injectGlobal`
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`;
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
---
+++ src/ui/base-kits/Text.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const Text = styled.p`
+  font-size: 1.2rem;
+  font-family: sans-serif;
+  color: palevioletred;
+`;
+
+export default Text;
