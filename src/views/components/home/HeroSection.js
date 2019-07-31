eec27e73e9c0e4ba535e9fb896c484cece4b8a98
Shahidul Islam Majumder <dev@shahid.pro>
2019-07-31 11:00:33 +0600

    Restructured project

---
+++ src/views/components/home/HeroSection.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const HeroSection = () => (
+  <>
+    <h1>Hello there,</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis fuga
+      aliquam autem vel. Cupiditate distinctio, consectetur quisquam unde qui,
+      nostrum nulla earum minima rem doloribus incidunt perferendis commodi
+      voluptatum, dolorum!
+    </p>
+    <p>
+      Go to <a href="/profile">profile</a>
+    </p>
+  </>
+);
+
+export default HeroSection;
--- src/views/pages/HomePage.js
+++ src/views/pages/HomePage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Button, Container } from '../components';
+import { HeroSection } from '../components/home';
 
 class HomePage extends Component {
   state = {
@@ -17,16 +18,7 @@ class HomePage extends Component {
   render() {
     return (
       <Container variant="fixed">
-        <h1>Hello there,</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis fuga
-          aliquam autem vel. Cupiditate distinctio, consectetur quisquam unde
-          qui, nostrum nulla earum minima rem doloribus incidunt perferendis
-          commodi voluptatum, dolorum!
-        </p>
-        <p>
-          Go to <a href="/profile">profile</a>
-        </p>
+        <HeroSection />
         <h4>Counter: {this.state.count}</h4>
         <Button onClick={this.handleIncrease} mr={16}>
           +
